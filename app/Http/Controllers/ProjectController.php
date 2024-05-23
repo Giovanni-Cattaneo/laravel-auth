c0dd4a4f43938a5cc1a1065a66f11a165ba1115c
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-23 09:23:10 +0200

    edit destroy method

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -88,6 +88,9 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        if ($project->cover_image) {
+            Storage::delete($project->cover_image);
+        }
         $project->delete();
 
         return to_route('admin.projects.index');
